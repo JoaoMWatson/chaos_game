1027b7928ceab8dc34fd8a23a11b27f8eb87bc76
Joao Pedro Watson <joaopedrowatson@hotmail.com>
2022-04-04 23:23:12 -0300

    mais correcao besta

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ screen.bgcolor('black')
 screen.colormode(255)
 
 
-turtle.speed(10)
+turtle.speed(100)
 turtle.color('white')
 turtle.penup()
 turtle.shape('circle')
@@ -17,7 +17,7 @@ turtle.shapesize(0.05, 0.05)
 P1 = (-200, -100)
 P2 = (200, -100)
 P3 = (0, 200)
-last_point = (0, 150)
+last_point = (0, 100)
 POINTS = [P1, P2, P3]
 
 turtle.goto(P1)
@@ -32,7 +32,7 @@ turtle.dot(5)
 turtle.goto(last_point)
 turtle.stamp()
 
-for i in range(0, 20100):
+for i in range(0, 20000):
     random_point = POINTS[randint(0, 2)]
     
     mid_point = (
@@ -45,3 +45,5 @@ for i in range(0, 20100):
     last_point = mid_point
     color = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
     turtle.color(color)
+
+screen.exitonclick()
